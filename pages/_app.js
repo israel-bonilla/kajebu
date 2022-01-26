b8c8424af1165105a222106b3b8a6219e4bd321f
coldthaw <israel1337b@gmail.com>
2022-01-26 06:25:43 -0600

    icon reload fix 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.css'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,13 +1,7 @@
-import { useEffect } from "react";
 import DashboardTemplate from "../components/Dashboard/DashboardTemplate"
 import WelcomeScreen from "../components/Dashboard/WelcomeScreen"
 
 const Dashboard = () => {
-  
-  useEffect(() => {
-    window.location.reload();
-  }, []);
-
   return <DashboardTemplate Screen={WelcomeScreen} />;
 }
 
