a1786a518ec2b131b57a24dc265ece6fba4edd41
coldthaw <israel1337b@gmail.com>
2022-07-22 19:39:45 -0500

    more styling

--- components/MarketingModule/EmailCampaignsScreen.js
+++ components/MarketingModule/EmailCampaignsScreen.js
@@ -1,12 +1,91 @@
-import { faEnvelopeOpenText } from "@fortawesome/free-solid-svg-icons"
+import { faEnvelopeOpenText, faRetweet, faSearch } from "@fortawesome/free-solid-svg-icons"
+import { faEnvelopeOpen, faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styles from "../../styles/Marketing/EmailCampaignsScreen.module.scss"
 
 const EmailCampaignsScreen = () => {
+  const CampaignRow = ({ icon, name, details, stats }) => (
+    <div className={styles.campaignRow}>
+      <FontAwesomeIcon icon={icon} className={styles.campaignRowIcon} />
+      <div className={styles.campaignRowMiddle}>
+        <p>{name}</p>
+        <p>{details}</p>
+      </div>
+      {stats && (
+        <div className={styles.campaignStats}>
+          <div>
+            <p>0</p>
+            <p>Sends</p>
+          </div>
+          <div>
+            <p>-</p>
+            <p>Opened</p>
+          </div>
+          <div>
+            <p>-</p>
+            <p>Clicked</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+  
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <h1 style={{ fontWeight: '500', margin: 20 }}>Email Campaigns</h1>
-      <FontAwesomeIcon icon={faEnvelopeOpenText} size="10x" color="gray" />
-      {/* Table */}
+    <div className={styles.container}>
+      {/* Header */}
+      <div className={styles.header}>
+        <h1>Email Campaigns</h1>
+        <FontAwesomeIcon icon={faQuestionCircle} size="lg" color="gray" />
+      </div>
+
+      {/* Nav */}
+      <div className={styles.nav}>
+        <ul className={styles.tabs}>
+          <li>All</li>
+          <li>Draft</li>
+          <li>Scheduled</li>
+          <li>Sent</li>
+          <li>Sequence</li>
+        </ul>
+
+        <div className={styles.search}>
+          <FontAwesomeIcon icon={faSearch} size="sm" color="gray" />
+          <input type="text" placeholder="Search Email Campaigns..." />
+        </div>
+      </div>
+
+      {/* Campaigns */}
+      <div className={styles.campaignRows}>
+        {
+          [
+            {
+              icon: faRetweet,
+              name: "Freebie Email Sequence",
+              details: "1 email over 0 days",
+              stats: true,
+            },
+            {
+              icon: faRetweet,
+              name: "Sales Page OVO Email Sequence",
+              details: "8 emails over 14 days",
+              stats: true,
+            },
+            {
+              icon: faEnvelopeOpen,
+              name: "Email Broadcast",
+              details: "Updated January 1, 2030",
+              stats: false,
+            },
+          ]
+          .map(cr => <CampaignRow key={cr.name} icon={cr.icon} name={cr.name} details={cr.details} stats={cr.stats} />)
+        }
+      </div>
+
+      {/* Footer */}
+      <div className={styles.footer}>
+        <FontAwesomeIcon icon={faQuestionCircle} color="blue" />
+        <p>Learn More about <span style={{ color: "blue" }}>Email Campaigns</span></p>
+      </div>
     </div>
   )
 }
--- components/WebsiteModule/DesignScreen.js
+++ components/WebsiteModule/DesignScreen.js
@@ -3,6 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from '../../styles/Website/DesignScreen.module.scss';
 
 const DesignScreen = () => {
+  const WebDesignRow = ({ title }) => (
+    <li className={styles.listItem}>{title}<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
+  );
+  const webDesignRowTitles = ["Homepage", "Library", "Store", "Sales Page", "Login", "Static Page", "Thank You Page", "404 Page", "Blog", "Blog Post", "Blog Search"];
+
   return <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
     <h1 style={{ fontWeight: '500', margin: 20 }}>Website Design</h1>
     <div className={styles.options}>
@@ -10,18 +15,8 @@ const DesignScreen = () => {
       <FontAwesomeIcon icon={faPalette} /><span>Manage Themes</span>
       <FontAwesomeIcon icon={faCompass} /><span>Navigation</span>
     </div>
-    <ul>
-      <li className={styles.listItem}>Homepage<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Library<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Store<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Sales Page<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Login<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Static Page<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Thank You Page<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>404 Page<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Blog<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Blog Post<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
-      <li className={styles.listItem}>Blog Search<FontAwesomeIcon icon={faPen} color="#347BFF" /></li>
+    <ul className={styles.webDesignList}>
+      {webDesignRowTitles.map(title => <WebDesignRow key={title} title={title} />)}
     </ul>
   </div>;
 };
